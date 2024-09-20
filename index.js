6d5153a56cd3d047359520404e5ed9501a5cded7
Voichita Maria <sabo_voichita@yahoo.com>
2024-09-20 11:30:21 +0200

    add a button and more quotes

--- index.js
+++ index.js
@@ -101,4 +101,38 @@ function updateTestimonial() {
   }, 5000);
 }
 
+function quoteButtonActivated() {
+  const quotes = [
+    "‘Do something you love everyday! - Maria T. ’",
+    "‘Defiéndete. Elige ser un guerrero en un mundo de subvencionados y burócratas - Bogdan T.’",
+    "‘Never stop loving! - Ana M.’",
+    "‘Love youself!’ - Juan G.",
+    "‘La pereza nos roba la energía - Bogdan T.’",
+    "‘Smile every day!’ - Natali B.",
+    "‘El conformismo sofoca todo inpulso - Bogdan T.’",
+    "‘Gather love around you!’ - Dina P.",
+    "‘El pesimismo es como una nube que oculta el sol - Bogdan T.’",
+    "‘Failure and invention are inseparable twins. To invent you have to experiment, and if you know in advance that it's going to work, it's not an experiment.’ - Jeff Bezos",
+    "‘Anyone can make the simple complicated. Creativity is making the complicated simple.’ - Charles Mingus",
+    "‘Muestra acción, voluntad y ambición - Bogdan T.’",
+    "‘Iteration is the fundamental basis of all improvement. - Mark Manson’",
+    "‘If you don't change your direction, you may end up where you are going. - Lao Tzu’",
+    "‘Fantasy is a necessary ingredient in living, it's a way of looking at life through the wrong end of a telescope. - A. Einstein’",
+    "‘We are drowning in information, while starving for wisdom. - E.O. Wilson’",
+    "‘The only way to do great work is to love what you do. - Steve Jobs’",
+    "‘Believe you can and you're halfway there. - Theodore Roosevelt’",
+    "‘It does not matter how slowly you go as long as you do not stop. - Confucius’",
+    "‘You are never too old to set another goal or to dream a new dream. - C.S. Lewis’",
+    "‘Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll’",
+  ];
+
+  document.getElementById("quoteButton").addEventListener("click", function () {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const [quoteText, username] = quotes[randomIndex].split(" - ");
+    document.getElementById("quotetext").innerText = quoteText;
+    document.querySelector(".username").innerText = username || "";
+  });
+}
+
 updateTestimonial();
+quoteButtonActivated();
